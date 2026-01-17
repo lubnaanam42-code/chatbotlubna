5ec048f135f53639bb152601e53068fcd9372fa3
lubnaanam42-code <lubna.anam42@gmail.com>
2026-01-18 02:27:09 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,59 @@
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_groq import ChatGroq
+import os
+
+# load env variables
+load_dotenv()
+
+# streamlit page setup
+st.set_page_config(
+    page_title="Chatbot",
+    page_icon="🤖",
+    layout="centered",
+)
+st.title("💬 Generative AI Chatbot")
+
+# debug (remove later)
+st.write("Groq key loaded:", os.getenv("GROQ_API_KEY")[:8])
+
+# initiate chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# show chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# LLM init (FIXED)
+llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    model_name="llama-3.1-8b-instant",
+    temperature=0.0,
+)
+
+# input box
+user_prompt = st.chat_input("Ask Chatbot...")
+
+if user_prompt:
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append(
+        {"role": "user", "content": user_prompt}
+    )
+
+    response = llm.invoke(
+        [
+            {"role": "system", "content": "You are a helpful assistant"},
+            *st.session_state.chat_history
+        ]
+    )
+
+    assistant_response = response.content
+
+    st.session_state.chat_history.append(
+        {"role": "assistant", "content": assistant_response}
+    )
+
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
